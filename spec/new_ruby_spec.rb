33e813db69f7963838e4882be167c5b7ced0fc12
MrKoBo <kdtate4@memphis.edu>
2024-03-16 03:47:10 -0500

    Finished testing on queue class

--- spec/new_ruby_spec.rb
+++ spec/new_ruby_spec.rb
@@ -17,7 +17,8 @@ RSpec.describe Stack do
     it "pushes a node item into the stack" do
       stack = Stack.new
       stack.push(1)
-      expect(stack.to_s).to eq("[1]")
+      stack.push(2)
+      expect(stack.to_s).to eq("[2 1]")
     end
   end
 
@@ -45,4 +46,44 @@ RSpec.describe Stack do
     expect(stack.is_empty?).to be true
     end
   end
+end
+
+RSpec.describe Queue do
+  describe "#enqueue" do
+    it "queues up the next item in the queue" do
+      queue = Queue.new
+      queue.enqueue(1)
+      queue.enqueue(2)
+      queue.enqueue(3)
+      expect(queue.to_s).to eq("[1 2 3]")
+    end
+  end
+
+  describe "#dequeue" do
+    it "it removes the oldest item from the queue" do
+      queue = Queue.new
+      queue.enqueue(1)
+      queue.enqueue(2)
+      queue.enqueue(3)
+      queue.dequeue
+      expect(queue.to_s).to eq("[2 3]")
+    end
+  end
+  describe "#is_empty?" do
+    it "returns true when the queue is empty" do
+    queue = Queue.new
+    expect(queue.is_empty?).to be true
+    end
+  end
+
+  describe "#to_s" do
+    it "returns a string representation of the queue" do
+      queue = Queue.new
+      queue.enqueue(10)
+      queue.enqueue(34)
+      queue.enqueue(57)
+      expect(queue.to_s).to eq("[10 34 57]")
+    end
+  end
+
 end
